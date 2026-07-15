3c15f9cf12e458fcd8812a4b761449e91a81418a
mimic <mimic@jiyu.com>
2026-07-15 16:08:13 +0800

    fix(server): 修正服务器地址及子模块引用更新
    - 更新libs/hbb_common子模块的

--- src/common.rs
+++ src/common.rs
@@ -1080,7 +1080,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.200.28".to_owned()
 }
 
 #[inline]
